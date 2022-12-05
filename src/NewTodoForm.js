85bf38f34b097bc67e0bc29efde90ea0b6fbeac0
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-12-05 12:19:08 -0600

    add input change handler

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewTodoForm.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const NewTodoForm = () => {
+  const [formData, setFormData] = useState('');
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+  };
   return (
     <div className="todo-form-container">
       <form className="todo-form">
-        <label>Enter Todo:</label>
-        <input placeholder="todo" className="todo-form-input"></input>
+        <label htmlFor="todo">Enter Todo:</label>
+        <input
+          id="todo"
+          type="text"
+          name="todo"
+          value={formData.todo}
+          placeholder="Enter Todo Item"
+          className="todo-form-input"
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
